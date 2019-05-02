a3cf6884cf050508776d4edc18a7e215a021425a
JKGravelle <gravellejk@g.cofc.edu>
2019-05-02 04:53:41 -0400

    final push

--- app/src/main/java/com/example/lab_07/MainActivity.java
+++ app/src/main/java/com/example/lab_07/MainActivity.java
@@ -1,44 +1,10 @@
 package com.example.lab_07;
 
-import android.content.Intent;
-import android.net.Uri;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.InputFilter;
-import android.text.TextWatcher;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.ListView;
-import android.widget.ProgressBar;
-import android.widget.Toast;
 
-import com.firebase.ui.auth.AuthUI;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import edu.cofc.briggs.myapplication.R;
-import edu.cofc.briggs.myapplication.adapter.MessageAdapter;
-import edu.cofc.briggs.myapplication.model.FriendlyMessage;
 
 public class MainActivity extends AppCompatActivity {
 
